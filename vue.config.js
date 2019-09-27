4e403ff69f1523d2a09619e46b044a763234e8b4
jianggushi <rogerjiangx@outlook.com>
2019-09-27 19:37:28 +0800

    add dev server cors

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,19 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/short-url/'
     : '/',
-  
+  // devServer
+  devServer: {
+    open: true,
+    proxy: {
+      '/weibo-api2': {
+        target: 'https://api.weibo.com/2',
+        changeOrigin: true,
+        pathRewrite: {
+            '^/weibo-api2': ''
+        }
+      }
+    }
+  },
   // webpack
   configureWebpack: {
     externals: {
