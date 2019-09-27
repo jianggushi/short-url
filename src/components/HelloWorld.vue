8e3df4aceb46c57a1c06ba2138a50b8b368301a1
jianggushi <rogerjiangx@outlook.com>
2019-09-27 08:01:17 +0800

    update image

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <el-button>Button</el-button>
+    <el-input v-model="input" placeholder="请输入内容" clearable style="width:50%"></el-input>
+    <p>{{value}}</p>
+    <el-button @click="convert">Button</el-button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      input: '',
+      value: ''
+    }
+  },
+  methods: {
+    convert() {
+      var url = 'https://api.weibo.com/2/short_url/shorten.json?source=560331235&url_long=' + this.input
+      console.log(url)
+      axios.get(url)
+        .then(resp => {
+          // console.log(resp)
+          this.value = resp
+        })
+        .catch(error => {
+          // console.log(resp)
+          alert(error)
+        })
+    }
   }
 }
 </script>
